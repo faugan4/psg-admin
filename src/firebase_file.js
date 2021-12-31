7555124e000142082ab648ace56aa4d3d43864bd
faugan <foganbidi@gmail.com>
2021-12-31 10:26:13 +0000

    second

--- src/firebase_file.js
+++ src/firebase_file.js
@@ -1,5 +1,5 @@
 import firebase from "firebase";
-
+/*
 const firebaseConfig = {
   apiKey: "AIzaSyBbZbzP407pWBHaaAEr48jJkrZm_8F2vfQ",
   authDomain: "prosportguru-a0569.firebaseapp.com",
@@ -7,6 +7,14 @@ const firebaseConfig = {
   storageBucket: "prosportguru-a0569.appspot.com",
   messagingSenderId: "46274020109",
   appId: "1:46274020109:web:77138d23dbfecebac1a644"
+};*/
+const firebaseConfig = {
+  apiKey: "AIzaSyC20eE_3FGGhzVCL-Hnx2ye0fskZ-UElrs",
+  authDomain: "prosport-guru-inc.firebaseapp.com",
+  projectId: "prosport-guru-inc",
+  storageBucket: "prosport-guru-inc.appspot.com",
+  messagingSenderId: "384851572120",
+  appId: "1:384851572120:web:6b31c6017fb9b4ac6367ed"
 };
 
 let app;
